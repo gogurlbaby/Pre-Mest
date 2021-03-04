4d204c73990441b5c582d6fb5ff196d4af221c29
gogurlbaby <philiahammmond@gmail.com>
2021-03-04 12:59:06 +0000

    Modified js and added vscode

--- js/main.js
+++ js/main.js
@@ -3,3 +3,44 @@ console.log("hellooo.....")
 let b = 78
 
 console.log(b);
+
+//Variables
+let myname = "Philia Hammond"
+console.log(myname);
+
+let a = 5
+b = 9
+console.log(a+b);
+console.log(a*b);
+
+let surname = "Hammond"
+let firstname  = "Philia"
+console.log(surname+firstname);
+
+//Arrays and Objects
+
+//Arrays
+let myarray = [8,4,10,13,34,54,90]
+let names = ["Kofi", "Kwasi", "Ama"]
+let mymixed = [0,4,6,"Yaw","Yam", "Banana"]
+
+console.log(myarray[2]);
+console.log(myarray[3]);
+console.log(myarray[6]);
+
+//myarray.push(50)
+//myarray.pop()
+myarray.pop(3)
+myarray.splice(0,3)
+console.log(myarray);
+
+//Ojects
+let myobject = {
+    name:"Kofi", 
+    class:3, 
+    classmate:"Mary Gold", 
+    results:[
+    {subjects:"English", mark:40, position:1},
+    {subjects:"Maths", mark:80, position:4},
+    {subjects:"Social", mark:50, position:3}
+    ]
